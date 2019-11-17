972cd04cec6cc3b5998e0ac8833143db4fa0a909
nayomi <nayomi.arvell@gmail.com>
2019-11-17 20:41:23 +0100

    Skriver ut ordrar mer tydligt

--- scripts/orderHistory.js
+++ scripts/orderHistory.js
@@ -4,69 +4,71 @@
 //sen vill jag skriva ut order arrayen med order-arrayerna i en omsluten div
 
 class OrderHistory {
-      constructor() {
-        this.addOrderListener();
+  constructor() {
+    this.addOrderListener();
+  }
+
+  addOrderListener() {
+    let orderBtn = document.querySelector(".order-btn");
+    let orderPrint = document.querySelector(".orderHistory");
+
+    if (!orderBtn) {
+      return;
+    }
+
+    orderBtn.addEventListener("click", e => {
+      if (!store.length) {
+        let getStoredArray =
+          JSON.parse(localStorage.getItem("OrderHist")) || [];
+        let newOrder = JSON.parse(localStorage.getItem("store"));
+        getStoredArray.push(newOrder);
+        localStorage.setItem("OrderHist", JSON.stringify(getStoredArray));
+      } else {
       }
 
-   addOrderListener() {
-   let orderBtn = document.querySelector(".order-btn");
-   let orderPrint = document.querySelector(".orderHistory");
-   
-  
-      if (!orderBtn) {
-        return;
+      function printOrderHistory() {
+        let getOrderHistoryArray =
+          JSON.parse(localStorage.getItem("OrderHist")) || [];
+        orderPrint.innerHTML = `
+        <div class="row">
+          <div class="col-sm-4 col-md-2 col-xl-1 mb-4">
+          <hr>
+            <h5>Order history</h5>
+          </div>
+        </div>
+        ${getOrderHistoryArray
+          .reverse()
+          .map(
+            ({ cart }) => `
+            <div class="row col-xl-12 mb-5 border">
+              <div class="col-sm-6 col-md-4 col-xl-4">
+                <p>${"Purchase date: " + new Date().toLocaleDateString()}<p>
+              </div>
+              <div class="col-sm-6 col-md-4 col-xl-2">
+                ${cart
+                  .map(
+                    ({ price }) => `
+                <p>${"Price: " + price}</p>`
+                  )
+                  .join("")}
+              </div>
+              <div class="col-sm-6 col-md-4 col-xl-4">
+                ${cart
+                  .map(
+                    ({ name }) => `
+                    <p>${"Package: " + name}</p>`
+                  )
+                  .join("")}
+              </div>
+              <hr>
+              </div>
+              `
+          )
+          .join("")}
+              `;
       }
-   
-      orderBtn.addEventListener("click", e => {
-        if(!store.length) {
-          let getStoredArray = JSON.parse(localStorage.getItem("OrderHist")) || [];
-          let newOrder = JSON.parse(localStorage.getItem("store"));
-          getStoredArray.push(newOrder);
-          localStorage.setItem('OrderHist', JSON.stringify(getStoredArray))
-        }else {
-    
-        }
-
-        function printOrderHistory() {
-          let getOrderHistoryArray = JSON.parse(localStorage.getItem("OrderHist")) || [];
-          orderPrint.innerHTML = `
-                      <div class="row">
-                        <div class="col-sm-4 col-md-2 col-xl-1 mb-4">
-                          <h5>Order history</h5>
-                        </div>
-                      </div>
-                      ${getOrderHistoryArray
-                        .reverse()
-                        .map(
-                          ({ date, cart}) => `
-                          <div class="row col-xl-12">
-                            <div class="col-sm-6 col-md-4 col-xl-4">
-                              <p>${"Purchase date: " + new Date().toLocaleDateString()}<p>
-                            </div>
-                            <div class="col-sm-6 col-md-4 col-xl-2">
-                            ${cart.map(({price}) => `
-                            <p>${"Price: " + price}</p>`
-                           ).join("")}
-                          </div>
-                            <div class="col-sm-6 col-md-4 col-xl-4">
-                              ${cart.map(({name}) => `
-                                <p>${"Package: " + name}</p>`
-                               ).join("")}
-                            </div>
-                            <hr>
-                          </div>
-                            `
-                        )
-                        .join("")}
-                     `;
-        }
-        printOrderHistory();
-     
-    });   
-   };
-
-
-   
-
+      printOrderHistory();
+    });
+  }
 }
 new OrderHistory();
